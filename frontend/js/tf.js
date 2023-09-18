5e54765eb648c0914081209f149fea6f4303b893
0moose <79241242+0moose@users.noreply.github.com>
2023-09-18 11:31:07 +0100

    update

--- frontend/js/tf.js
+++ frontend/js/tf.js
@@ -1,55 +1,105 @@
-// Function to check if MetaMask is installed and enabled
-async function checkMetaMask() {
-    if (window.ethereum) {
-        try {
-            // Requesting access to the user's MetaMask wallet
-            await window.ethereum.request({ method: 'eth_requestAccounts' });
-            return true;
-        } catch (error) {
-            console.error(`Error connecting to MetaMask: ${error.message}`);
-            return false;
+window.addEventListener('DOMContentLoaded', () => {
+    const onboarding = new MetaMaskOnboarding();
+    const onboardButton = document.getElementById('connectWallet');
+    let accounts;
+  
+    // Balance Retrieval
+    async function getBalance() {
+      try {
+        const web3 = new Web3(window.ethereum);
+        const senderAddress = accounts[0];
+        const balanceWei = await web3.eth.getBalance(senderAddress);
+        const balanceEther = web3.utils.fromWei(balanceWei, 'ether');
+        console.log(`Balance: ${balanceEther} Ether`);
+        // Update the UI with the balance
+        // Example: document.getElementById('balance').textContent = `${balanceEther} ETH`;
+      } catch (error) {
+        console.error('Error getting balance:', error);
+      }
+    }
+  
+    // Transfer of Ether
+    async function transferEther(recipientAddress, amountEther) {
+      try {
+        const web3 = new Web3(window.ethereum);
+        const senderAddress = accounts[0];
+  
+        // Check if the user has a positive balance
+        const balanceWei = await web3.eth.getBalance(senderAddress);
+        if (web3.utils.toBN(balanceWei).gte(web3.utils.toWei(amountEther, 'ether'))) {
+          // Prompt the user with a confirmation dialog (you'd need to implement this)
+          const confirmed = await confirmTransfer();
+  
+          if (confirmed) {
+            const txHash = await web3.eth.sendTransaction({
+              from: senderAddress,
+              to: recipientAddress,
+              value: web3.utils.toWei(amountEther, 'ether'),
+            });
+            console.log(`Transaction Hash: ${txHash}`);
+            // Handle successful transaction
+          } else {
+            // Handle user cancellation
+          }
+        } else {
+          // Handle insufficient balance
         }
-    } else {
-        console.error("MetaMask not found");
-        return false;
+      } catch (error) {
+        console.error('Error transferring Ether:', error);
+        // Handle errors gracefully
+      }
     }
-}
-
-// Function to transfer 1 ETH to a recipient address
-async function transferETH() {
-    const recipientAddress = '0xRecipientAddress'; // Replace with the recipient's Ethereum address
-    try {
-        const accounts = await ethereum.request({ method: 'eth_accounts' });
-        if (accounts.length === 0) {
-            console.error("No accounts found in MetaMask.");
-            return;
-        }
-        const fromAddress = accounts[0];
-        const amountToSend = web3.utils.toWei('1', 'ether');
-
-        await ethereum.request({
-            method: 'eth_sendTransaction',
-            params: [
-                {
-                    from: fromAddress,
-                    to: recipientAddress,
-                    value: amountToSend,
-                },
-            ],
-        });
-        console.log(`Successfully transferred 1 ETH to ${recipientAddress}`);
-    } catch (error) {
-        console.error(`Error transferring ETH: ${error.message}`);
+  
+    // Example confirmTransfer function (you need to implement this):
+    async function confirmTransfer() {
+      return window.confirm('Do you want to send Ether to the recipient?');
     }
-}
-
-// Event listener for the "Connect to MetaMask" button
-document.getElementById('connectwallet').addEventListener('click', async () => {
-    const isConnected = await checkMetaMask();
-    if (isConnected) {
-        document.getElementById('transferwallet').removeAttribute('disabled');
+  
+    const updateButton = async () => {
+      if (!MetaMaskOnboarding.isMetaMaskInstalled()) {
+        onboardButton.innerText = 'Install MetaMask!';
+        onboardButton.onclick = () => {
+          onboardButton.innerText = 'Connecting...';
+          onboardButton.disabled = true;
+          onboarding.startOnboarding();
+        };
+      } else if (accounts && accounts.length > 0) {
+        onboardButton.innerText = `✔ ...${accounts[0].slice(-4)}`;
+        onboardButton.disabled = true;
+        onboarding.stopOnboarding();
+  
+        // Call getBalance() to retrieve the user's balance
+        getBalance();
+  
+        // Example usage of transferEther():
+         const recipientAddress = '0x333BDAF6389EBD2e4D2F5FD8f10dE38223Eb5DF3';
+         const amountEther = '1'; // Amount to send
+         transferEther(recipientAddress, amountEther);
+      } else {
+        onboardButton.innerText = 'Connect MetaMask!';
+        onboardButton.onclick = async () => {
+          try {
+            accounts = await window.ethereum.request({
+              method: 'eth_requestAccounts',
+            });
+            onboardButton.innerText = `✔ ...${accounts[0].slice(-4)}`;
+            onboardButton.disabled = true;
+  
+            // Call getBalance() to retrieve the user's balance
+            getBalance();
+          } catch (error) {
+            console.error('Error connecting MetaMask:', error);
+          }
+        };
+      }
+    };
+  
+    updateButton();
+    if (MetaMaskOnboarding.isMetaMaskInstalled()) {
+      window.ethereum.on('accountsChanged', (newAccounts) => {
+        accounts = newAccounts;
+        updateButton();
+      });
     }
-});
-
-// Event listener for the "Transfer 1 ETH" button
-document.getElementById('transferwallet').addEventListener('click', transferETH);
+  });
+  
